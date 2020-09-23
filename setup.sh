9090203ec879b19059f0a57a0482eca1ae4fcd71
Huizi Mao <ralphmao95@gmail.com>
2020-09-23 12:23:08 -0700

    add prompt to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,11 @@
 set -e
 
+echo "Copying dock to $HOME/.local/bin"
 mkdir -p $HOME/.local/bin
 cp ./dock $HOME/.local/bin
 echo 'export PATH=$PATH:$HOME/.local/bin' >> $HOME/.bashrc
 
+echo "Copying config file to $HOME/.docker"
 mkdir -p $HOME/.docker
 cp ./dockpath $HOME/.docker
+echo "Done"
